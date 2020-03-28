da8c42cac00ca73bfc3b189aa3a033660fab6aa5
Ceren Karabulut <57019480+ceren-karabulut@users.noreply.github.com>
2020-03-28 12:43:40 +0300

    Add files via upload

---
+++ Northwind.DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,20 @@
+﻿using Northwind.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.DataAccess.Abstract
+{
+   public interface IEntityRepository<T> where T:class,IEntity,new() //Generic kısıtlama.
+       //Repository Generic:Gelecekte farklı metotların yazılması
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);     //filtre isterse yapar.
+        T Get(Expression<Func<T,bool>> filter);     //kullanıcı mutlaka filtre versin.Örneğin kategori id/product id göre 
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
